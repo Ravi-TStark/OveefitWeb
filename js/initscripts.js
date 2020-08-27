c11847ad8922a91d89dffde8da8b4b8a13832ce2
Ravi-TStark <58636984+Ravi-TStark@users.noreply.github.com>
2020-08-28 01:25:01 +0530

    Scroll Color Transition Isolated

--- js/initscripts.js
+++ js/initscripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('scroll', function(){
-    if(window.scrollY >= window.innerHeight - 100){
-        document.getElementById('header').style.backgroundColor = 'rgba(65,117,229,0.9)';
+    if(window.scrollY >= window.innerHeight - 500 && window.scrollY < 2 * window.innerHeight){
+        document.getElementById('header').style.backgroundColor = 'rgba(0 ,93 , 167, 0.6)';
     }
     else{
         document.getElementById('header').style.backgroundColor = 'transparent';
